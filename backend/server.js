d47469742e3c1d215983a0bdc5553d25b8d27059
Shanda Shaw <srh0029@hotmail.com>
2023-11-06 09:23:27 -0600

    trail photos added to assets & relative links to trail seed data

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,15 @@ const PORT = process.env.PORT||3001
 const cors = require('cors')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
- 
+
 const app = express()
- 
+
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json( ))
 app.use(express( ))
 app.use(express.urlencoded({extended: false}))
- 
+
 app.listen(PORT,() => {
-            console.log(`Express server listening on port ${PORT}`)
+    console.log(`Express server listening on port ${PORT}`)
 })
