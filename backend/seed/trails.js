ac62c43cb51f3568e2e6e45c333c8b8cd1e2fc7c
AnthonyBattista02 <tonybatt2002@gmail.com>
2023-11-06 10:30:48 -0600

    server.js + bugfixes

--- backend/seed/trails.js
+++ backend/seed/trails.js
@@ -1,7 +1,7 @@
 const db = require('../db')
 const Trail = require('../models/trail')
 
-db.on('error', consol.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async() => {
     const trails = [
@@ -49,7 +49,7 @@ const main = async() => {
             trailName: "St. Edwards Park: Hill and Creek Trails",
             miDistance: 1.2,
             difficulty: "Moderate",
-            hikingTime: 1.5,
+            hikingTimeHrs: 1.5,
             trailSurface: "Dirt, stones, limestone outcrops",
             feesPermits: "None",
             schedule: "5:00am to 10:00pm daily",
@@ -59,7 +59,7 @@ const main = async() => {
             trailName: "Wild Basin Wilderness Preserve",
             miDistance: 2.1,
             difficulty: "Moderate",
-            hikingTime: 2,
+            hikingTimeHrs: 2,
             trailSurface: "Dirt, limestone",
             feesPermits: "Voluntary day-use fee based on the honor system",
             schedule: "Sunrise to sunset daily",
@@ -69,7 +69,7 @@ const main = async() => {
             trailName: "Emma Long Metropolitan Park: Turkey Creek Trail",
             miDistance: 2.6,
             difficulty: "Moderate",
-            hikingTime: 1.5,
+            hikingTimeHrs: 1.5,
             trailSurface: "Dirt path",
             feesPermits: "None",
             schedule: "7:00am to 10:00pm daily",
@@ -79,7 +79,7 @@ const main = async() => {
             trailName: "Commons Ford: Lake Austin Loop",
             miDistance: 1.6,
             difficulty: "Easy",
-            hikingTime: .75,
+            hikingTimeHrs: .75,
             trailSurface: "Dirt, grass",
             feesPermits: "None",
             schedule: "1:0pam to 6:00pm Tuesday through Sunday",
@@ -89,7 +89,7 @@ const main = async() => {
             trailName: "Barton Creek Greenbelt: Gaines Creek Access",
             miDistance: 3.2,
             difficulty: "Moderate",
-            hikingTime: 2.5,
+            hikingTimeHrs: 2.5,
             trailSurface: "Dirt and rock",
             feesPermits: "None",
             schedule: "5:00am to 10:00pm daily",
@@ -99,7 +99,7 @@ const main = async() => {
             trailName: "Balcones Canyonlands National Wildlife Refuge: Cactus Rocks Trail",
             miDistance: 3.6,
             difficulty: "Moderate",
-            hikingTime: 2.5,
+            hikingTimeHrs: 2.5,
             trailSurface: "Dirt, gravel, limestone outcrop",
             feesPermits: "None",
             schedule: "Sunrise to sunset daily",
