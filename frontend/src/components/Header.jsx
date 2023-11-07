b91238ffe4e835e4ee7f7ea95498daa434f9870e
ahunter623 <ahunter623@gmail.com>
2023-11-07 17:02:42 -0500

    Nav/Header/CSS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
-import { useState, useEffect, useContext } from 'react'
-import { Link, useParams, useNavigate } from "react-router-dom"
-import axios from 'axios'
+// import { useState, useEffect, useContext } from 'react'
+// import { Link, useParams, useNavigate } from "react-router-dom"
+// import axios from 'axios'
+//import React from 'react'
 
-//import Nav from './Nav'
+import Nav from './Nav.jsx'
 
-const Header = () => {
-    return(
-        <h1>I am the Header</h1>
+export default function Header () {
+    return (
+        <header>
+        <div className="Header">
+            <h1>I am the Header!</h1>
+            
+        </div>
+        </header>
     )
-}
-
-export default Header
+}   
